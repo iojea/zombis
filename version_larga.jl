c1cdda58a939a6661faf6883503c2f3ad1e71c89
Ignacio Ojea <iojea@MacBook-Air-de-Ignacio.local>
2022-04-27 15:48:46 -0300

    short version

--- version_larga.jl
+++ version_larga.jl
@@ -18,7 +18,7 @@ end
 begin
 	using HypertextLiteral: @htl
 	using PlutoUI: CheckBox, Slider, Show
-	using Plots: plot, plot!, scatter, scatter!, theme
+	using Plots: plot, plot!, scatter, scatter!
 	theme(:wong)
 end
 
@@ -27,7 +27,7 @@ end
 
 	<style> 
 		.title {
-			background: #D0B485;
+			background: #EEEEEE;
 			border-radius: 12px;
 			text-align: center;
 			padding: 0px 10px 0px 10px;
@@ -35,8 +35,66 @@ end
 		}
 	</style>""")
 
+# ╔═╡ 696e597a-45f0-44b9-b47e-a877abb87ab0
+@htl("""<h2 class="title">Primera Parte</h2>""")
+
+# ╔═╡ 3a5b4a4f-7955-475f-9d02-448572a3e63a
+md"""Supongamos primero que todos los humanos vivimos en un edificio y salimos todos juntos para ver si hay más zombis. Cada vez que salimos, se infecta la mitad del grupo. Veamos cómo evoluciona la cantidad de humanos:"""
+
+# ╔═╡ 7e7c5630-0999-401a-94ea-4e921c96d936
+let
+	url = "http://mate.dm.uba.ar/%7Eiojea/figura.png"
+	data = read(download(url))
+	PlutoUI.Show(MIME"image/jpg"(), data)
+end
+
+# ╔═╡ 2a86adbe-972a-4dd9-845d-609b187879bf
+md"""Supongamos ahora que todos los humanos vivimos en un edificio y salimos todos juntos para ver si hay más zombis. Pero cada vez que salimos, se infecta la décima parte del grupo:
+
+ $\begin{array}{cccc}
+   h_0 & \phantom{aa} & h_1=h_0-\frac{h_0}{10} & \dots \\ 
+   t=0 & \phantom{aa} & t=1 & \dots \\
+  \end{array}$
+
+ ¿Cuántos humanos somos a tiempo $n$ con respecto a lo que había al principio?
+
+ $\begin{align*}
+ h_{n+1} &= h_n-\frac{1}{10}h_{n}\\ 
+         &= \bigg(\frac{9}{10}\bigg) h_n \\
+         &= \bigg(\frac{9}{10}\bigg)\bigg(\frac{9}{10}\bigg) h_{n-1} \\
+         &= \bigg(\frac{9}{10}\bigg)^2 h_{n-1} \\
+         &= \dots  \\
+         &= \bigg(\frac{9}{10}\bigg)^{n+1} h_0 
+   \end{align*}$
+
+ ¿Cómo será el gráfico de la cantidad de humanos en función del tiempo?"""
+
+# ╔═╡ 4800127a-30be-4594-bc4e-6bb6c9c3149b
+md"""Pensemos ahora en la cantidad de zombis que, cada vez que salen los humanos, aumenta (porque los humanos se convierten en zombis). Supongamos que estamos en un caso donde, cada vez que salen los humanos, la cantidad de zombis aumenta en un décimo:
+
+
+$\begin{array}{cccc}
+   z_0 & \phantom{aa} & z_1=z_0+\frac{z_0}{10} & \dots \\ 
+   t=0 & \phantom{aa} & t=1 & \dots \\
+  \end{array}$
+
+ ¿Cuántos zombis hay a tiempo $n$ con respecto a lo que había al principio? Razonamos como antes:
+
+ $z_{n+1} = z_n+\frac{1}{10}z_n = \Big(\frac{11}{10}\Big)z_n = \dots = \Big(\frac{11}{10}\Big)^{n+1} z_0$
+ 
+ ¿Cómo sería un gráfico de la cantidad de zombis en función del tiempo?"""
+
+# ╔═╡ c8262dd6-5c56-4786-be3a-9d37486a7c03
+md"""Conclusión, en cada caso tenemos que:
+
+$h_{n}=\alpha^n h_0, \hspace{2cm} z_{n}=\beta^n z_0,$
+
+para ciertos valores de $\alpha$ y $\beta$,  
+
+¿Qué podemos deducir del comportamiento cuando el tiempo $n$ se hace cada vez más grande?"""
+
 # ╔═╡ d52565c5-e983-40d2-ab65-c76bfc3fcc01
-@htl("""<h2 class="title">Crecimiento de poblaciones con natalidad y mortalidad</h2>""")
+@htl("""<h3 class="title">Crecimiento con natalidad y mortalidad</h3>""")
 
 # ╔═╡ 097d4c17-5966-4ef5-962d-c6545091e97c
 md"""
@@ -68,9 +126,9 @@ md"""
 md"""
  Z = $(@bind Z Slider(1000:10:10000,default=1000,show_value=true)) 
 
- N = $(@bind n Slider(0:0.01:1,default=0.5,show_value=true)) 
+ n = $(@bind n Slider(0:0.01:1,default=0.5,show_value=true)) 
 
- M = $(@bind m Slider(0:0.01:1,default=0.3,show_value=true))
+ m = $(@bind m Slider(0:0.01:1,default=0.3,show_value=true))
 """
 
 # ╔═╡ e4f95747-ab32-47df-bf4d-359aec4b81fc
@@ -94,9 +152,9 @@ md"""¿Qué ocurre si hay más contagios que muertes?
 # ╔═╡ 97a4a916-096b-4361-8d1b-0424a63d4edd
 md"""Recordemos que habíamos deducido que la cantidad de zombis se puede escribir como $z_n = \beta^n z_0$.
 
-En nuestro caso $\beta = (1+N-M)$, para conocer la población luego de $15$ años podríamos hacer directamente este cálculo:
+En nuestro caso $\beta = (1+n-m)$, para conocer la población luego de $15$ años podríamos hacer directamente este cálculo:
 
-$z_{15} = (1+N-M)^{14}\cdot 1000$
+$z_{15} = (1+0.2-0.1)^{14}\cdot 1000$
 
 Volvemos a hacer el gráfico anterior y agregamos el valor de $z_{15}$, destacado:"""
 
@@ -108,12 +166,108 @@ begin
 	scatter!([15],[z15],label="z15",marker=:star5,markersize=8)
 end
 
+# ╔═╡ 6bbd67c1-6b98-468d-8ff4-022cc5bc48e0
+@htl("""<p class="separator"></p>
+
+	<style> 
+		.separator {
+			height: 5px;
+			background-color:darkslategray;
+		}
+	</style>""")
+
 # ╔═╡ 3ef542df-44a1-43af-872e-1138c12eb17f
 @htl("""<h2 class="title">Competencia entre especies</h2>""")
 
+# ╔═╡ 51443cff-ad5e-4d21-98ac-3256198e7a11
+md"""2.   Pero la cantidad de zombis y la cantidad de humanos dependen una de la otra. Empecemos suponiendo simplemente que la cantidad de zombis aumenta proporcionalmente a la cantidad de humanos. Entonces: 
+$$\left\lbrace\begin{array}{rcl}h_{k+1} & = & h_k-\alpha h_k \\ z_{k+1} & = & z_k+\alpha h_k \end{array}\right.$$
+¿Cómo es la evolución de las poblaciones si, por ejemplo, $h_0=1000$, $z_0=1$, $\alpha = 0.05$?"""
+
+
+
+# ╔═╡ 3179374a-be70-4025-8510-f5986a779539
+md"""
+ z₀ = $(@bind z₀ Slider(1:10:100,default=1,show_value=true)) 
+
+ h₀ = $(@bind h₀ Slider(1000:10:10000,default=1000,show_value=true)) 
+
+ α = $(@bind α Slider(0:0.01:1,default=0.05,show_value=true)) 
+"""
+
+# ╔═╡ d6f488cb-79f5-4b97-a0ae-2065822d7f67
+md"""$(@bind gtot CheckBox()) Población total"""
+
+
+
+# ╔═╡ b7d5379b-ce50-41c5-bc31-cb37c777ac80
+begin
+	h = zeros(16)
+	z = zeros(16)
+	h[1] = h₀
+	z[1] = z₀
+	for i in 2:16
+		h[i] = h[i-1] - α*h[i-1]
+		z[i] = z[i-1] + α*h[i-1]
+	end
+	if gtot
+		scatter(0:15,z,legend=:top,label="zombis")
+		scatter!(0:15,h,label="humanos")
+		scatter!(0:15,z+h,label="total")
+	else
+		scatter(0:15,z,legend=:top,label="zombis")
+		scatter!(0:15,h,label="humanos")
+	end
+end
+
+# ╔═╡ 9831f016-6497-4984-bc54-749b112cd384
+md"""3.   También podemos pensar que la cantidad de humanos decrece de manera proporcional a la cantidad de zombis, mientras que los zombis aumentan de manera proporcional a la cantidad de humanos:
+$$\left\lbrace\begin{array}{rcl}h_{k+1} & = & h_k-\alpha z_k \\ z_{k+1} & = & z_k+\beta h_k \end{array}\right.$$
+¿Cómo varía la cantidad de humanos si hay muchos zombis? ¿Y si hay pocos?<br> ¿Qué pasa con los zombis? ¿Y después de un largo tiempo, qué pasa?
+
+Probamos con $h_0=1000$, $z_0=10$, $\alpha=0.01$ y $\beta=0.05$.
+
+¿Qué ocurre con la cantidad total de individuos (humanos + zombis)?
+
+¿Y si se invierten los valores de $\alpha$ y $\beta$?"""
+
+# ╔═╡ 5487a745-e091-42ea-9e37-4b929df74822
+md"""
+ z₀ = $(@bind z2₀ Slider(1:3:100,default=10,show_value=true)) 
+
+ h₀ = $(@bind h2₀ Slider(1000:10:10000,default=1000,show_value=true)) 
+
+ α = $(@bind α2 Slider(0:0.01:1,default=0.05,show_value=true)) 
+ 
+ β = $(@bind β2 Slider(0:0.01:1,default=0.05,show_value=true)) 
+"""
+
+# ╔═╡ 444eb6b1-fc1d-4f82-ad0a-c19fc2511284
+md"""$(@bind gtot2 CheckBox()) Población total"""
+
+# ╔═╡ 2208fc1f-fed7-40ca-83a6-e24f6d374eae
+begin
+	h2 = zeros(16)
+	z2 = zeros(16)
+	h2[1] = h2₀
+	z2[1] = z2₀
+	for i in 2:16
+		h2[i] = h2[i-1] - α2*z2[i-1]
+		z2[i] = z2[i-1] + β2*h2[i-1]
+	end
+	if gtot2
+		scatter(0:15,z2,legend=:top,label="zombis")
+		scatter!(0:15,h2,label="humanos")
+		scatter!(0:15,z2+h2,label="total")
+	else
+		scatter(0:15,z2,legend=:top,label="zombis")
+		scatter!(0:15,h2,label="humanos")
+	end
+end
+
 # ╔═╡ 6b2ea6e3-8a31-4032-8f2f-4088c78fa15a
 md""" 
-2.  Veamos un modelo más complejo. Consideremos lo siguiente:
+5.  Veamos un modelo más complejo. Consideremos los siguiente:
 
   *   Los humanos nacemos proporcionalmente a nuestra población (a tasa $N$),
   *   Para que un humano se convierta en zombi, se tiene que encontrar un humano con un zombi, tiene que haber interacción y hay una tasa de efectividad con la que los zombis logran contagiar a los humanos ($a$),
@@ -172,7 +326,7 @@ end
 
 # ╔═╡ 7014b7a0-62c9-48d5-9aaa-4e84053da67e
 md"""
-3. Se puede observar una dinámica interesante si contemplamos la posibilidad de que los zombis mueran por "causas naturales". En ese caso, las ecuaciones quedan:
+6. Se puede observar una dinámica interesante si contemplamos la posibilidad de que los zombis mueran por "causas naturales". En ese caso, las ecuaciones quedan:
 
 $\left\lbrace\begin{array}{rcl}h_{k+1} & = & h_k+(N-M) h_k - a h_k z_k \\ 
 z_{k+1} & = & c z_k+(a-b) h_k z_k \\
@@ -786,9 +940,9 @@ uuid = "a63ad114-7e13-5084-954f-fe012c677804"
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
 
 [[deps.NaNMath]]
-git-tree-sha1 = "b086b7ea07f8e38cf122f5016af580881ac914fe"
+git-tree-sha1 = "737a5957f387b17e74d4ad2f440eb330b39a62c5"
 uuid = "77ba4419-2d1f-58cd-9bb1-8ffee604a2e3"
-version = "0.3.7"
+version = "1.0.0"
 
 [[deps.NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
@@ -828,9 +982,9 @@ version = "8.44.0+0"
 
 [[deps.Parsers]]
 deps = ["Dates"]
-git-tree-sha1 = "3b429f37de37f1fc603cc1de4a799dc7fbe4c0b6"
+git-tree-sha1 = "1285416549ccfcdf0c50d4997a94331e88d68413"
 uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
-version = "2.3.0"
+version = "2.3.1"
 
 [[deps.Pixman_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
@@ -1246,6 +1400,12 @@ version = "0.9.1+5"
 # ╔═╡ Cell order:
 # ╟─ac901706-a470-47ba-8351-f9e87869ab9a
 # ╟─ec7ab690-a52c-11ec-1e8a-f77727daef1a
+# ╟─696e597a-45f0-44b9-b47e-a877abb87ab0
+# ╟─3a5b4a4f-7955-475f-9d02-448572a3e63a
+# ╟─7e7c5630-0999-401a-94ea-4e921c96d936
+# ╟─2a86adbe-972a-4dd9-845d-609b187879bf
+# ╟─4800127a-30be-4594-bc4e-6bb6c9c3149b
+# ╟─c8262dd6-5c56-4786-be3a-9d37486a7c03
 # ╟─d52565c5-e983-40d2-ab65-c76bfc3fcc01
 # ╟─097d4c17-5966-4ef5-962d-c6545091e97c
 # ╟─ed49e3ed-dd07-44bb-b5a5-ef86c9332859
@@ -1254,7 +1414,16 @@ version = "0.9.1+5"
 # ╟─b12e6b22-0626-46d5-9608-480f454712f3
 # ╟─97a4a916-096b-4361-8d1b-0424a63d4edd
 # ╟─c1f2e4dd-c4ca-43fa-9fe8-8f2a8436893e
+# ╟─6bbd67c1-6b98-468d-8ff4-022cc5bc48e0
 # ╟─3ef542df-44a1-43af-872e-1138c12eb17f
+# ╟─51443cff-ad5e-4d21-98ac-3256198e7a11
+# ╟─3179374a-be70-4025-8510-f5986a779539
+# ╟─d6f488cb-79f5-4b97-a0ae-2065822d7f67
+# ╟─b7d5379b-ce50-41c5-bc31-cb37c777ac80
+# ╟─9831f016-6497-4984-bc54-749b112cd384
+# ╟─5487a745-e091-42ea-9e37-4b929df74822
+# ╟─444eb6b1-fc1d-4f82-ad0a-c19fc2511284
+# ╟─2208fc1f-fed7-40ca-83a6-e24f6d374eae
 # ╟─6b2ea6e3-8a31-4032-8f2f-4088c78fa15a
 # ╟─b979ab88-6cf4-4782-b45b-e812cf87ba36
 # ╟─ba00baca-f3b2-4ee4-8332-547d3590a2b8
