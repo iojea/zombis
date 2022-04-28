4d7d0d820b1f9df70f4550ef2825440bf0b995ae
Ignacio Ojea <iojea@MacBook-Air-de-Ignacio.local>
2022-04-27 22:06:05 -0300

    create sys image path

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,6 +1,6 @@
 using PackageCompiler
 create_sysimage([:Pluto, :PlutoUI, :Plots, :HypertextLiteral];
-                precompile_execution_file = "warmup.jl",
-                replace_default = true,
+                #precompile_execution_file = "warmup.jl",
+                #replace_default = true,
                 sysimage_path="/home/jovyan/sysimage.so",
                 cpu_target = PackageCompiler.default_app_cpu_target())
